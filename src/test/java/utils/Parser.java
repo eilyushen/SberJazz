07bb41af0887b7c8fb7659a0ea94dafbc0096cfa
EgorIlyushchenkov <egor.ilyushchenkov@ugasoft.com>
2023-09-04 03:50:51 +0500

    3

---
+++ src/test/java/utils/Parser.java
@@ -0,0 +1,25 @@
+package utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Parser {
+
+    public List<String> listOfNames() {
+        String path = "src\\test\\resources\\userNames";
+
+        List<String> lines = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(path))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                lines.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+}
